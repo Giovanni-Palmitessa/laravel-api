18e2909bd343b9022271348d146178034c6af829
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-14 15:44:16 +0200

    create portfolio controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::get('portfolios', [PortfolioController::class, 'index'])->name('api.portfolios.index');
