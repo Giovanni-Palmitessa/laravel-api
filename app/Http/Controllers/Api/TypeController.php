cd5ac9f296270ee96e58a1b83c87465ebfde5e1d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-18 18:01:11 +0200

    add method in controller for index

--- app/Http/Controllers/Api/TypeController.php
+++ app/Http/Controllers/Api/TypeController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Type;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TypeController extends Controller
 {
-    //
+    public function index() {
+        $types = Type::all();
+
+        return response()->json([
+            'success' => true,
+            'results' => $types,
+        ]);
+    }
 }
