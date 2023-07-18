9fbace269a8dd8f53f9e5061e09f56157e801380
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-18 16:36:24 +0200

    add new rout for random projects

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ use App\Http\Controllers\Api\PortfolioController;
 // });
 
 Route::get('portfolios', [PortfolioController::class, 'index'])->name('api.portfolios.index');
+Route::get('portfolios/random', [PortfolioController::class, 'random'])->name('api.portfolios.random');
 Route::get('portfolios/{portfolio}', [PortfolioController::class, 'show'])->name('api.portfolios.show');
