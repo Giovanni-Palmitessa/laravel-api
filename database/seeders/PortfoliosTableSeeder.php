c897e3c28d7b5eca9c83640ca12b93fe0ef0eda5
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-18 16:05:59 +0200

    adjust seeder for images

--- database/seeders/PortfoliosTableSeeder.php
+++ database/seeders/PortfoliosTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Type;
 use App\Models\Portfolio;
 use App\Models\Technology;
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -20,12 +21,15 @@ class PortfoliosTableSeeder extends Seeder
     {
         for ($i = 0; $i < 50; $i++) {
             $types = Type::all();
+            $name = $faker->words(rand(2, 4), true);
             $technologies = Technology::all()->pluck('id');
+            $imageIndex = rand(0,6);
             $portfolio = Portfolio::create([
                 'type_id' => $faker->randomElement($types)->id,
-                'name' => $faker->words(rand(2, 4), true),
+                'name' => Str::ucfirst($name),
                 'client_name' => $faker->words(2, true),
                 'url_image' => 'https://picsum.photos/id/' . rand(1, 270) . '/500/400',
+                'image' => $imageIndex ? 'uploads/picsum' . $imageIndex : null,
                 'pickup_date' => $faker->date(),
                 'deploy_date' => $faker->date(),
                 'description' => $faker->paragraphs(rand(2, 20), true),
